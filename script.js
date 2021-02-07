3dfc9e30ef5e4534716cf2c427ebec97979f5ae5
Moisés Pacífico <moises.pacifico@hotmail.com>
2021-02-06 23:45:41 -0300

    Add live validation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+const inputGroup = [username, email, password, password2];
 
 // Show input error message
 function showError (input, message) {
@@ -43,7 +44,6 @@ function checkLength (input, min, max) {
 //Check if valid password
 function checkPassword(input) {
     const mediumRegex = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}$");
-    console.log(mediumRegex);
 
     if(input.value.trim() === "") {
         showError(input, `${getFieldName(input)} is required`);
@@ -71,11 +71,48 @@ function getFieldName(input) {
 }
 
 // Event Listener
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+// form.addEventListener("keyup", (e) => {
+//     checkLength(username, 3, 15);
+//     checkEmail(email);
+//     checkPassword(password);
+//     checkPasswordMatch(password, password2);
+// });
 
-    checkLength(username, 3, 15);
-    checkEmail(email);
-    checkPassword(password);
-    checkPasswordMatch(password, password2);
+let timer,
+    timeoutVal = 800; // time it takes to wait for user to stop typing in ms
+
+// Username live validation
+username.addEventListener('keypress', () => {window.clearTimeout(timer)});
+username.addEventListener('keyup', () => {
+    window.clearTimeout(timer); // prevent errant multiple timeouts from being generated
+  timer = window.setTimeout(() => {
+      checkLength(username, 3, 15)
+  }, timeoutVal)
+});
+
+// Email live validation
+email.addEventListener('keypress', () => {window.clearTimeout(timer)});
+email.addEventListener('keyup', () => {
+    window.clearTimeout(timer); // prevent errant multiple timeouts from being generated
+  timer = window.setTimeout(() => {
+      checkEmail(email)
+  }, timeoutVal)
+});
+
+// Live password validation
+password.addEventListener('keypress', () => {window.clearTimeout(timer)});
+password.addEventListener('keyup', () => {
+    window.clearTimeout(timer); // prevent errant multiple timeouts from being generated
+  timer = window.setTimeout(() => {
+      checkPassword(password)
+  }, timeoutVal)
+});
+
+// Live password confirmation
+password2.addEventListener('keypress', () => {window.clearTimeout(timer)});
+password2.addEventListener('keyup', () => {
+    window.clearTimeout(timer); // prevent errant multiple timeouts from being generated
+  timer = window.setTimeout(() => {
+      checkPasswordMatch(password, password2)
+  }, timeoutVal)
 });
